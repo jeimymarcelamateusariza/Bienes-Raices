e68e823bd32aae06cd3961a85b4f1e2c6c3df630
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-07-29 19:45:16 -0500

    Conectando con la base de datos

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
+import db from "./config/db.js";
 
 //Crear la app
 const app = express();
+
+//Conectar a la base de datos
+try {
+    await db.authenticate();
+    console.log("Conexión a la base de datos exitosa");
+} catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+}
 
 //Habilitar PUG
 app.set("view engine", "pug");
