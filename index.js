9c606af7ae7a943bf0519d81cf6599f2b0facf9a
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-09-08 19:30:55 -0500

    feat: dependecias

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/auth", userRoutes);
 
 
 //Definir puerto y arrancar el servidor
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
